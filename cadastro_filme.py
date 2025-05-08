5bc9cd09279b062c180f11f7feef9f7c61b203d1
Suedson <76700706+suedsonn@users.noreply.github.com>
2025-05-07 21:20:31 -0300

    projeto novo

---
+++ cadastro_filme.py
@@ -0,0 +1,85 @@
+import time
+from loguru import logger
+
+
+bd_filmes = [
+    ['Matrix', 1997],
+    ['Vingadores', 2015]
+]
+
+def cadastra_filmes(bd, titulo, ano):
+    filme = [titulo, ano]
+    bd.append(filme)
+    return bd
+
+def lista_filmes(bd):
+    for i in range(len(bd)):
+        logger.info('Listagem de Filmes')
+        print(f'{i+1} | {bd[i][1]} | {bd[i][0]}')
+
+def altera_filme(bd, indice, titulo, ano):
+    bd[indice][0] = titulo
+    bd[indice][1] = ano
+    return bd
+
+def salvar_filmes(bd):
+
+    with open('bd_filmes.txt', 'w', encoding='utf-8') as arquivo:
+
+        for i in range(len(bd)):
+            logger.info(f'Salvando o filme {bd[i][0]}')
+            arquivo.write(f'{bd[i][1]}, {bd[i][0]}\n')
+
+
+while True:
+    print('1 - Cadastrar Filme')
+    print('2 - Listar Filmes')
+    print('3 - Alterar Filme')
+    print('4 - Salvar Filmes')
+    try:
+     op = int(input('Digite sua opção: '))
+    except Exception as e:
+        logger.error(f'Error:{e}')
+        logger.info('Digite um valor númerico')
+        op = -1
+
+    if op == 1:
+        logger.info('Iniciando o cadastro do filme')
+
+        titulo = input('Digite o título do filme: ')
+        ano = int(input('Digite o ano do filme: '))
+
+        bd_filmes = cadastra_filmes(
+            bd=bd_filmes,
+            titulo=titulo,
+            ano=ano
+        )
+
+        logger.info('Filme cadastrado com sucesso')
+
+
+    elif op == 2:
+        logger.info('Iniciado listagem de filmes')
+        lista_filmes(bd_filmes)
+        logger.info('Filmes listados com sucesso!')
+
+    elif op == 3:
+        lista_filmes(bd_filmes)
+        i = int(input('Qual filme deseja alterar? '))
+        titulo = input('Digite o novo título: ')
+        ano = int(input('Digite o novo ano: '))
+        bd_filmes = altera_filme(
+            bd=bd_filmes,
+            indice=(i-1),
+            titulo=titulo,
+            ano=ano
+        )
+        logger.info('Filme alterado com sucesso!')
+    elif op ==4:
+        logger.info('Iniciando persistência de filmes')
+        salvar_filmes(bd_filmes)
+        logger.info('Filmes salvos com sucesso!')
+    else:
+        print(f'Opção {op} inválida!')
+
+    time.sleep(3)
